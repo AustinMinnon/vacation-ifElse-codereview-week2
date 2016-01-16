b7a5eb4d8b8c6efa903f21ae3159344713693cd0
austin <whi7ee@gmail.com>
2016-01-15 16:27:43 -0800

    add better indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
-$(document).ready(function(){
-  $("form#questions").submit(function(event) {
-    var social = $("select#social").val();
-    var region = $("select#region").val();
-    var activity = $("select#activity").val();
-    var age = $("select#age").val();
-    var gender = $("select#gender").val();
-      if (social === "solo") {
-        $("#cancunid").show();
-        }else if (social === "date") {
-          $("#parisid").show();
-        }else if (social === "fam") {
-          $("#orlandoid").show();
+$(document).ready(function() {
+    $("form#questions").submit(function(event) {
+        var social = $("select#social").val();
+        var region = $("select#region").val();
+        var activity = $("select#activity").val();
+        var age = $("select#age").val();
+        var gender = $("select#gender").val();
+        if (social === "solo") {
+            $("#cancunid").show();
+        } else if (social === "date") {
+            $("#parisid").show();
+        } else if (social === "fam") {
+            $("#orlandoid").show();
         }
-event.preventDefault()
+        event.preventDefault()
     });
-      });
+});
 
-        ;
+;
